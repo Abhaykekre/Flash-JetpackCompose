bd34b61e3084630a4f7078c3ea019c60c7034152
Abhay Kekre <abhaykekre9@gmail.com>
2024-08-10 20:39:02 +0530

    creating FlashApp and StartScreen file

---
+++ app/src/main/java/com/example/flash/ui/StartScreen.kt
@@ -0,0 +1,9 @@
+package com.example.flash.ui
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun StartScreen() {
+     Text(text = "Hello World")
+}
