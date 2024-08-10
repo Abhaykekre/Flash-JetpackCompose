8185651407b1f72d496dfaa93f88945143d190ca
Abhay Kekre <abhaykekre9@gmail.com>
2024-08-10 22:02:03 +0530

    creating FlashApp and StartScreen file

---
+++ app/src/main/java/com/example/flash/data/DataSource.kt
@@ -0,0 +1,4 @@
+package com.example.flash.data
+
+object DataSource {
+}
--- app/src/main/java/com/example/flash/ui/StartScreen.kt
+++ app/src/main/java/com/example/flash/ui/StartScreen.kt
@@ -1,9 +1,41 @@
 package com.example.flash.ui
 
+import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.flash.R
 
 @Composable
 fun StartScreen() {
-     Text(text = "Hello World")
+     val context = LocalContext.current
+     Card (modifier = Modifier.clickable {
+          Toast.makeText(context, "This card was clicked", Toast.LENGTH_SHORT).show()
+     }) {
+          Column (
+               modifier = Modifier.padding(5.dp)
+          ) {
+               Text(
+                    text = "Hello World",
+                    fontSize = 24.sp,
+                    modifier = Modifier.width(150.dp)
+               )
+          }
+          Image(
+               painter = painterResource(id = R.drawable.fruits),
+               contentDescription = "" ,
+               modifier = Modifier.size(150.dp)
+          )
+     }
 }
