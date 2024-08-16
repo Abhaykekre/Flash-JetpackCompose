4286512dde512c6d8fbe95a7c22977d71053cfb5
Abhay Kekre <abhaykekre9@gmail.com>
2024-08-16 13:09:37 +0530

    conntecting our app to internet

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,6 @@ android {
     }
 
 
-
 }
 
 dependencies {
@@ -72,12 +71,16 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.manifest)
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
     implementation("androidx.navigation:navigation-compose:2.6.0")
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-scalars:2.11.0")
-    implementation("'com.squareup.retrofit2:retrofit:2.11.0'")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
 
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
-    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.1")
+    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
+
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0 ")
+
+
+
 }
--- app/src/main/java/com/example/flash/network/FlashApiService.kt
+++ app/src/main/java/com/example/flash/network/FlashApiService.kt
@@ -1,18 +1,21 @@
 package com.example.flash.network
 
 import com.example.flash.data.InternetItem
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import retrofit2.Retrofit
 import retrofit2.http.GET
 
+
 private const val BASE_URL = "https://training-uploads.internshala.com"
 
 private val retrofit = Retrofit.Builder().addConverterFactory(
     Json.asConverterFactory(
-      "application/json".toMediaType()
+        "application/json".toMediaType()
     )
 ).baseUrl(BASE_URL).build()
+
 
 interface FlashApiService {
     @GET("android/grocery_delivery_app/items.json")
